a11aa1d0b3951b82b30cf33bf4e2d3891896aae4
Daniel Hara <haradaniel734@gmail.com>
2024-07-06 22:46:26 +0200

    set up conditions

--- image.js
+++ image.js
@@ -1,15 +1,11 @@
 const images = document.getElementsByTagName('img');
 
 for (const image of images) {
+    if (image.naturalHeight > 1.5* image.height || image.naturalWidth > 1.5 * image.width) {
+        const warningText = document.createElement('div');
+        warningText.innerText = 'YOU MUST RESIZE THE IMAGE ABOVE!'
+        warningText.style = "background-color:red;font-size:40px;"
 
-    console.log(image.height, 'image.height');
-
-    console.log(image.naturalWidth, 'image.naturalWidth');
-    console.log(image.naturalHeight, 'image.naturalHeight');
-
-    const warningText = document.createElement('div');
-    warningText.innerText = 'YOU MUST RESIZE THE IMAGE ABOVE!'
-    warningText.style = "background-color:red;font-size:40px;"
-
-    image.parentNode.insertBefore(warningText, image.nextSibling)
+        image.parentNode.insertBefore(warningText, image.nextSibling)
+    }
 }
