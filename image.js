0971e040d00092aae4dd9d507fa0c4ce2cb3d71c
Daniel Hara <haradaniel734@gmail.com>
2024-07-06 22:39:53 +0200

    show warning

--- image.js
+++ image.js
@@ -1,14 +1,15 @@
-alert('launching');
-
 const images = document.getElementsByTagName('img');
 
-console.log('launching!')
-console.log('images', images);
-
 for (const image of images) {
-    console.log(image.width, 'image.width');
+
     console.log(image.height, 'image.height');
 
     console.log(image.naturalWidth, 'image.naturalWidth');
     console.log(image.naturalHeight, 'image.naturalHeight');
+
+    const warningText = document.createElement('div');
+    warningText.innerText = 'YOU MUST RESIZE THE IMAGE ABOVE!'
+    warningText.style = "background-color:red;font-size:40px;"
+
+    image.parentNode.insertBefore(warningText, image.nextSibling)
 }
